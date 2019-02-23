a7cf8b03dd924a390375b0c16b0774e8b62414a9
fengna <fengna@iwubida.com>
2019-02-23 11:40:17 +0800

    Add custom filter

---
+++ src/main/java/com/na/springboot/configurations/FilterConfig.java
@@ -0,0 +1,48 @@
+package com.na.springboot.configurations;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 自定义Filter
+ *
+ * @author fengna
+ * @date 19/2/23 11:21
+ */
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        return new RemoteIpFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean testFilterRegistration() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("name", "nana");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    public class MyFilter implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            System.out.println("this is MyFilter,url :" + request.getRequestURI());
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
