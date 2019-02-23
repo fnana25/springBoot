d4a2d5f463fe58e3a8a1060aee444ebe37b3b869
fengna <fengna@iwubida.com>
2019-02-23 10:54:53 +0800

    Add more swagger info

--- src/main/java/com/na/springboot/controller/IndexController.java
+++ src/main/java/com/na/springboot/controller/IndexController.java
@@ -1,18 +1,17 @@
 package com.na.springboot.controller;
 
 import com.na.springboot.controller.models.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * IndexController
+ * 首页
  *
  * @author fengna
  * @date 19/2/21 21:33
  */
 @RestController
+@RequestMapping("index")
 @Api(description = "首页相关API")
 public class IndexController {
 
@@ -20,8 +19,8 @@ public class IndexController {
      * 首页
      * @return hello
      */
+    @GetMapping
     @ApiOperation(value="首页", notes="首页展示")
-    @GetMapping("index")
     public String index(){
 
         return "hello";
@@ -31,11 +30,15 @@ public class IndexController {
      * 获取user
      * @return user
      */
+    @GetMapping("user/{id}")
     @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
-    @GetMapping("user/{id}")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "参数有误"),
+            @ApiResponse(code = 404, message = "路径有误")
+    })
     public User getUser(@PathVariable Long id){
 
-        return new User(1L,"fengna","692098869@qq.com");
+        return new User(id,"fengna","692098869@qq.com");
     }
 }
